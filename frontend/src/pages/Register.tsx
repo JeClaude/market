552311d1e41be83243e2cee08e23cb6628cc69d2
JeClaude <87715102+JeClaude@users.noreply.github.com>
2026-03-16 23:47:21 +0200

    another update

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -27,7 +27,7 @@ const Register = () => {
     setErrorMessage("");
     
     try {
-        const response = await fetch(`${API_URL}/api/auth/register`, {
+        const response = await fetch(`${API_URL}api/auth/register`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
